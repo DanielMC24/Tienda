caf3005cced2a0b0ad467c596219aa410c337ef8
Usuario <Usuario@Daniel-Escritorio>
2023-06-21 18:27:03 -0600

    Semana6

--- src/main/java/com/Tienda/service/CategoriaService.java
+++ src/main/java/com/Tienda/service/CategoriaService.java
@@ -10,4 +10,15 @@ public interface CategoriaService {
     //Metodo para consultar las categorias. El parametro define si se muestran solo las activas o todas
     public List<Categoria> getCategorias(boolean activos);
     
+    // Se obtiene una categoria por su ID
+    public Categoria getCategoria(Categoria categoria);
+    
+    //Insertar, se inserta cuando el id categoria esta vacio (valor 0 o null)
+    //Modificar, se modifica cuando el id categoria no esta vacio
+    public void save(Categoria categoria);
+    
+    //Se elimina un registro por su idCategoria
+    public void delete(Categoria categoria);
+    
+    
 }
