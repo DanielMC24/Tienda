d95e08a48ce2979cfc539cdfecf70b704054696f
Usuario <Usuario@Daniel-Escritorio>
2023-07-19 08:36:47 -0600

    Semana 10

--- src/main/java/com/Tienda/controller/IndexController.java
+++ src/main/java/com/Tienda/controller/IndexController.java
@@ -1,20 +1,36 @@
 
 package com.Tienda.controller;
 
+import com.Tienda.dao.UsuarioDao;
+import com.Tienda.domain.Usuario;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- *
- * @author Usuario
- */
+
 @Controller
 public class IndexController {
     
+    @Autowired
+    UsuarioDao usuarioDao;
+    
     @RequestMapping("/")
-    public String page(Model model) {
-       // model.addAttribute("attribute", "value");
+    public String page(Model model, HttpSession httpSession) {
+//       // Obtener usuario loggeado
+//       Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//       
+//       UserDetails user = null;
+//       
+//       if (principal instanceof UserDetails){
+//       
+//           Usuario usuario = usuarioDao.findByUsername(user.getUsername());
+//           
+//           httpSession.setAttribute("Email", usuario.getCorreo());
+//       }
         return "index";
     }
     
